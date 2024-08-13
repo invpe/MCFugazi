5be8e7018c5a98e77c4f82b6526639670a5d916a
invpe <106522950+invpe@users.noreply.github.com>
2024-08-13 13:51:31 +0200

    Update paper-install.sh

--- Server/paper-install.sh
+++ Server/paper-install.sh
@@ -1,6 +1,15 @@
-sudo apt install -y apt-transport-https ca-certificates wget gnupg2
-wget -qO - https://packages.sury.org/php/apt.gpg | sudo gpg --dearmour -o /usr/share/keyrings/sury-php.gpg
-echo "deb [signed-by=/usr/share/keyrings/sury-php.gpg] https://packages.sury.org/java/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-java.list
-sudo apt update
-sudo apt install -y openjdk-21-jdk
+#!/bin/bash
+clear
+echo "Installing PAPER MC"
+echo "1. Downloading paper"
+wget https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/13/downloads/paper-1.21.1-13.jar
+echo "2. Installing Java"
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install ca-certificates apt-transport-https gnupg wget
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt-get update
+sudo apt-get install -y java-21-amazon-corretto-jdk libxi6 libxtst6 libxrender1
+echo "3. Checking for Java"
 java -version
+echo "4. No errors, you're done for this setup"
